a5a4aa8e33f93c8197162a672141e2ecd046422e
AnarchyBurger <n.l.schneider@gmail.com>
2018-07-09 13:57:13 -0400

    first commit

--- js/updateJS.js
+++
@@ -1,5 +0,0 @@
-
-function updateJS() {
-    HEATAPI.Auth.getSignInfo(function (data) { alert(data.login) ;
-        document.getElementById('userInfo').innerHTML = '<li><span class="glyphicon glyphicon-user"></span> Hello ' + data.login + '!</li>'
-} ) } 
